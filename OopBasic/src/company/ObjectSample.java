21e66aa89ecc64a12fe513c2b67414559269c550
dir-FY24hirakuritakuma <takuma.hirakuri.g229@dirbato.co.jp>
2024-04-08 10:57:24 +0900

    L3 C4.3

--- OopBasic/src/company/ObjectSample.java
+++ OopBasic/src/company/ObjectSample.java
@@ -8,6 +8,12 @@ public class ObjectSample {
         
        employee.report();
         employee.report(2);
+        
+        System.out.println("");
+        
+        var engineer = new Employee("田中", "開発部", "一般社員", 88);
+        engineer.report();
+        
 
     }
 
